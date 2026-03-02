2b2667c24152909f628221a2b6f171fb6c61654b
Sanjay Saravanan <sanjays@datatemplate.com>
2026-03-02 10:34:57 +0530

    Updates the url

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors({
     "http://localhost:2000",
     "https://appointment-frontend-6jd3.vercel.app",
     "https://appointment-frontend-admin-vn4f.vercel.app",
-    "https://appointment-provider-skml.vercel.app"
+    "https://appointment-provider-2000.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
